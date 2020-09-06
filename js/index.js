38b6ad2c7322f9db7ef4b1502b6c3f9bf25b45c5
trent <b.trent5@gmail.com>
2020-09-06 13:28:47 -0600

    significant progress

--- js/index.js
+++ js/index.js
@@ -1,55 +1,3 @@
-const rollDice = () => {
-    const diceFaces = {
-        d00: `ASPFFK`,
-        d01: `NUIHMQ`,
-        d02: `OBJOAB`,
-        d03: `LNHNRZ`,
-        d10: `AHSPCO`,
-        d11: `RYVDEL`,
-        d12: `IOTMUC`,
-        d13: `LREIXD`,
-        d20: `TERWHV`,
-        d21: `TSTIYD`,
-        d22: `WNGEEH`,
-        d23: `ERTTYL`,
-        d30: `OWTOAT`,
-        d31: `AEANEG`,
-        d32: `EIUNES`,
-        d33: `TOESSI`
-    }
-
-    const diceElements = Array.from(document.getElementsByClassName(`dice`)).sort(() => Math.random() - 0.5)
-
-    const dice = Object.keys(diceFaces).map((key) => {
-        diceElements.pop().id = key
-        return new Die({faces: diceFaces[key], id: key})
-    })
-    dice.forEach((die) => die.roll())
-}
-
-const boggle = () => {
-    rollDice()
-    fadeBackground()
-}
-
-const fadeBackground = () => {
-    const gameTime = 3 * 60
-
-    const fadeToRed = () => {
-        document.body.style.cssText += `transition: background-color ${gameTime}s`
-        document.body.style.cssText += `background-color: red`
-    }
-
-    const resetToWhite = () => {
-        document.body.style.removeProperty(`background-color`)
-        document.body.style.removeProperty(`transition`)
-    }
-
-    document.body.style.cssText += `background-color: green`
-    setTimeout(fadeToRed)
-    setTimeout(resetToWhite, gameTime * 1000)
-}
-
 class Die {
     constructor({faces, id}) {
         this.faces = Array.from(faces)
@@ -59,9 +7,85 @@ class Die {
     roll() {
         const elem = document.getElementById(this.id)
         const face = this.faces[Math.floor(Math.random() * this.faces.length)]
+        this.rotate(parseInt(`${Math.random() * 4}`))
         if (face == `Q`) elem.innerText = `Qu`
         else elem.innerText = face
     }
+
+    rotate(turns) {
+        const elem = document.getElementById(this.id)
+        elem.style.removeProperty(`transform`)
+        elem.style.cssText += `transform: rotate(${90 * turns}deg)`
+    }
 }
 
-Array.from(document.getElementsByClassName(`dice`)).forEach((die) => die.innerText = `?`)
+const diceFaces = {
+    d00: `ASPFFK`,
+    d01: `NUIHMQ`,
+    d02: `OBJOAB`,
+    d03: `LNHNRZ`,
+    d04: `AHSPCO`,
+    d05: `RYVDEL`,
+    d06: `IOTMUC`,
+    d07: `LREIXD`,
+    d08: `TERWHV`,
+    d09: `TSTIYD`,
+    d10: `WNGEEH`,
+    d11: `ERTTYL`,
+    d12: `OWTOAT`,
+    d13: `AEANEG`,
+    d14: `EIUNES`,
+    d15: `TOESSI`
+}
+
+const diceElements = () => Array.from(document.getElementsByClassName(`dice`))
+
+const dice = () => {
+    const elems = diceElements().sort(() => Math.random() - 0.5)
+    return Object.keys(diceFaces).map((key) => {
+        elems.pop().id = key
+        return new Die({faces: diceFaces[key], id: key})
+    })
+}
+
+const rollDice = () => dice().forEach((die) => die.roll())
+const concealDice = () => diceElements().forEach((die) => die.innerText = `?`)
+const straightenDice = () => dice().forEach((die) => die.rotate(0))
+
+const resetDice = () => {
+    concealDice()
+    straightenDice()
+}
+
+const resetBackground = () => {
+    document.body.style.removeProperty(`background-color`)
+    document.body.style.removeProperty(`transition`)
+    document.body.style.cssText += `background-color: slategray`
+}
+
+const resetGame = () => {
+    resetDice()
+    resetBackground()
+    document.getElementById(`start-button`).disabled = false
+}
+
+const boggle = (gameSeconds) => {
+    rollDice()
+
+    document.body.style.cssText += `transition: background-color ${gameSeconds}s`
+    document.body.style.cssText += `background-color: red`
+
+    document.getElementById(`start-button`).disabled = true
+}
+
+const setup = () => {
+    document.body.addEventListener(`transitionend`, () => {
+        resetBackground()
+        document.getElementById(`start-button`).disabled = false
+    })
+
+    concealDice()
+    resetBackground()
+}
+
+setup()
